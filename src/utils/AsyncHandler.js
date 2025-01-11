1ee1870a7507205cf68cbe3be1f9af94305fa4f7
pranavyamjal <yamjal0188@gmail.com>
2025-01-11 17:29:16 +0530

    Add utility classes for API error handling and responses

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,11 @@
+const AsyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+export default AsyncHandler;
