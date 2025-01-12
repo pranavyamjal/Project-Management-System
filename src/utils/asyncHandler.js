a96298f1a2fea4040fef94d942c97e619200ee3f
pranavyamjal <yamjal0188@gmail.com>
2025-01-12 23:01:08 +0530

    Add fixed file

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {  // Returns middleware function
+        Promise.resolve(requestHandler(req, res, next))  // Converts handler to Promise
+        .catch((err) => next(err))  // Passes any errors to Express error handler
+    }
+}
+
+ 
+export default asyncHandler;
